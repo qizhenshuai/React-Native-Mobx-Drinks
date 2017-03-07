b8102bc44260ce4285216ce975097410d9e9293a
ethan <ethan@ethandeMacBook-Pro.local>
2017-03-08 01:56:07 +0800

    start project in today

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React,{Component} from 'react';
+import {
+    View,
+    Text,
+    Image
+} from 'react-native';
+
+class App extends Component{
+    render(){
+        return(
+            <View>
+                <Text>使用react-native-mobx编写RN商城开源项目</Text>
+            </View>
+        )
+    }
+}
+
+export default App;
